4b018a68fc1a4c4d0df77556672b446450077c3f
Warkung <warkung@gmail.com>
2025-05-26 14:50:09 +0700

    EP2 Navbar

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,18 @@
+import Logo from "./Logo";
+import Search from "./Search";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="container flex flex-col justify-between py-8 sm:flex-row sm:items-center gap-4  ">
+        <Logo />
+        <Search />
+        <div className=" flex">
+          <h1>Darkmode</h1>
+          <h1>Profile</h1>
+        </div>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
